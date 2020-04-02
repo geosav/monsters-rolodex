585098a8f7a7907d11ee85d526b0008bc1f4c86a
Yurii Savchuk <yurii.savchuk@ukietech.com>
2020-04-02 22:58:19 +0300

    Updates

---
+++ precache-manifest.b2a4bb6122c36585318d65fad7a52153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fdc424d24fccc3c25f7127bd0821b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c54902df3b7de0a6a0a1",
+    "url": "/monsters-rolodex/static/css/main.f69b3c56.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54902df3b7de0a6a0a1",
+    "url": "/monsters-rolodex/static/js/main.cba76088.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
